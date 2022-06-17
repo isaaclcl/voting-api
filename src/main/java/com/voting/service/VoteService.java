2760e406a40034d6875a89e79b73ede554fff109
Isaac Leal <isaacleal@gmail.com>
2022-06-17 09:53:41 -0300

    Adding a basic kafka config

--- src/main/java/com/voting/service/VoteService.java
+++ src/main/java/com/voting/service/VoteService.java
@@ -8,13 +8,21 @@ import com.voting.modal.projection.VoteCount;
 import com.voting.repository.SessionRepository;
 import com.voting.repository.VoteRepository;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import static java.util.Objects.isNull;
 
@@ -22,11 +30,16 @@ import static java.util.Objects.isNull;
 @RequiredArgsConstructor
 @Getter
 @Setter
+@Slf4j
 public class VoteService {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     private final VoteRepository voteRepository;
 
     private final SessionRepository sessionRepository;
+    @Value(value = "${kafka.voteTopic}")
+    private String voteTopic;
 
     public Vote newVote(Vote vote) {
         if (isNull(vote.getSession())) {
@@ -56,4 +69,25 @@ public class VoteService {
         voteCounts.add(new VoteCount("NAO", nao));
         return voteCounts;
     }
+
+    public void sendMessage(String message) {
+
+        ListenableFuture<SendResult<String, String>> future =
+                kafkaTemplate.send(voteTopic, message);
+
+        future.addCallback(new ListenableFutureCallback<>() {
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.debug(String.format("Sent message=[%s] with offset=[%s]",
+                        message, result.getRecordMetadata().offset()));
+            }
+
+            @Override
+            public void onFailure(@NonNull Throwable ex) {
+                log.error(String.format("Unable to send message=[%s] due to : %s", message, ex.getMessage()));
+            }
+        });
+    }
+
 }
